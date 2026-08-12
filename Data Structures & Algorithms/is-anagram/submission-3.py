37d80939ac92a9a0e9153c07d6f731314c7aa3cf
Charanjit Singh <129447119+charanjit2619@users.noreply.github.com>
2026-08-12 13:35:33 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_seen = {}
+        for letter in s:
+            if letter in s_seen:
+                s_seen[letter] += 1
+            else:
+                s_seen[letter] = 0
+
+        t_seen = {}
+        for letter in t:
+            if letter in t_seen:
+                t_seen[letter] += 1
+            else:
+                t_seen[letter] = 0
+
+        for key, value in t_seen.items():
+            if key in s_seen:
+                if value == s_seen[key]:
+                    continue
+                else:
+                    return False
+            else:
+                return False
+        return True
+        
